72b6786a76248dd734e1c34fc6aae9a5961cffab
jiyeon11 <leejiyeon415@gmail.com>
2023-10-24 19:42:27 +0900

    ✨ 설탕 코팅

--- fruit.h
+++ fruit.h
@@ -11,17 +11,20 @@ public:
     Sprite sprite;  // 과일 이미지
     Texture coatedTexture;  //코팅된 과일 이미지
     bool grabbed;   // 과일을 잡았는가
-    bool isCoating = false;  //설탕물 코팅했는가
+    bool isCoated = false;  //설탕물 코팅했는가
     string name;  //과일 이름
 
     //생성자
     Fruit(String fruitName) : name(fruitName) {
         grabbed = true;
+        coatedTexture.loadFromFile("image/Coating_" + name + ".png");
     }
 
     //설탕 코팅 이미지로 바꿔줌
     void sugarCoating() {
-        coatedTexture.loadFromFile("image/Coating_" + name + ".png");
-        sprite.setTexture(coatedTexture);
+        if (!isCoated) {
+            sprite.setTexture(coatedTexture);  //코팅된 이미지로 변경
+            isCoated = true;
+        }
     }
 };
--- game.cpp
+++ game.cpp
@@ -105,6 +105,7 @@ void Game::run(RenderWindow& window) {
     bool isLadleGrabbed = false;  //국자를 집었는가 안집었는가
     bool isSugarLadle = false;  //설탕물 국자인가 아닌가
     bool isStickGrabbed = false;  //꼬치를 집었는가 안집었는가
+    int maxSize = 0;  //꽂을 과일 갯수 정하는 변수
     vector<Fruit> fruits;  //과일 벡터
 
     //제한시간 : 초 설정
@@ -160,31 +161,90 @@ void Game::run(RenderWindow& window) {
     thread(AfterSale_btn).detach(); // 새 스레드에서 실행
 
     // 과일 주문 목록
-    vector<wstring> orders = {
-       L"딸기 탕후루 주세요.",
-        L"샤인머스켓 탕후루 주세요.",
-        L"파인애플 탕후루 주세요.",
-        L"블랙 사파이어 포도 탕후루 주세요.",
-        L"통귤 탕후루 주세요.",
-        L"딸기,샤인머스캣 탕후루 주세요.",
-        L"딸기,파인애플 탕후루 주세요.",
-        L"딸기,블랙 사파이어 포도 주세요.",
-        L"딸기,통귤 탕후루 주세요.",
-        L"샤인머스캣,파인애플 탕후루 주세요.",
-        L"샤인머스캣,블랙 사파이어 포도 주세요.",
-        L"샤인머스캣,통귤 탕후루 주세요.",
-        L"파인애플,블랙 사파이어 탕후루 주세요."
-        L"파인애플,통귤 탕후루 주세요."
-        L"블랙 사파이어 포도,통귤 탕후루 주세요"
+    vector<string> orders = { 
+        "strawberry",
+        "shinemusket", 
+        "pineapple",
+        "black grape", 
+        "mandarin",
+        "strawberry,shinemusket",
+        "strawberry,pineapple",
+        "strawberry,black grape",
+        "strawberry,mandarin",
+        "shinemusket,pineapple",
+        "shinemusket,black grape",
+        "pineapple,black grape",
+        "pineapple,mandarin",
+        "black grape,mandarin",
     };
 
+    wstring order;  //한글 주문용
+    
     //주문 랜덤 돌리기
     //시드 값을 현재 시간을 이용하여 생성
     mt19937 rng(random_device{}());
     // orders에서 랜덤한 주문을 선택
     uniform_int_distribution<int> dist(0, orders.size() - 1);
     int randomIndex = dist(rng); // 랜덤한 인덱스 생성
-    bubbleText.setString(orders[randomIndex]);
+
+    //주문 wstring으로 변환
+    if (orders[randomIndex] == "strawberry") {
+        order = L"딸기";
+        maxSize = 4;
+    }else if (orders[randomIndex] == "shinemusket") {
+        order = L"샤인머스켓";
+        maxSize = 7;
+    }
+    else if (orders[randomIndex] == "pineapple") {
+        order = L"파인애플";
+        maxSize = 5;
+    }
+    else if (orders[randomIndex] == "black grape") {
+        order = L"블랙 사파이어 포도";
+        maxSize = 15;
+    }
+    else if (orders[randomIndex] == "mandarin") {
+        order = L"통귤";
+        maxSize = 4;
+    }
+    else if (orders[randomIndex] == "strawberry,shinemusket") {
+        order = L"딸기,샤인머스켓";
+        maxSize = 5;
+    }
+    else if (orders[randomIndex] == "strawberry,pineapple") {
+        order = L"딸기,파인애플";
+        maxSize = 5;
+    }
+    else if (orders[randomIndex] == "strawberry,black grape") {
+        order = L"딸기,블랙 사파이어 포도";
+        maxSize = 6;
+    }
+    else if (orders[randomIndex] == "strawberry,mandarin") {
+        order = L"딸기,통귤";
+        maxSize = 4;
+    }
+    else if (orders[randomIndex] == "shinemusket,pineapple") {
+        order = L"샤인머스켓,파인애플";
+        maxSize = 6;
+    }
+    else if (orders[randomIndex] == "shinemusket,mandarin") {
+        order = L"샤인머스켓,통귤";
+        maxSize = 5;
+    }
+    else if (orders[randomIndex] == "pineapple,black grape") {
+        order = L"파인애플,블랙 사파이어";
+        maxSize = 7;
+    }
+    else if (orders[randomIndex] == "pineapple,mandarin") {
+        order = L"파인애플,통귤";
+        maxSize = 4;
+    }
+    else if (orders[randomIndex] == "black grape,mandarin") {
+        order = L"블랙 사파이어 포도,통귤";
+        maxSize = 6;
+    }
+
+    bubbleText.setString(order + L" 탕후루 주세요.");
 
     while (window.isOpen()) {
         Event event;
@@ -200,7 +260,7 @@ void Game::run(RenderWindow& window) {
 
             //커서가 과일 박스 위에 있다면 그 과일을 잡음
             if (blackGrapeBox.getGlobalBounds().contains(static_cast<Vector2f>(mousePosition))) {  //블랙 사파이어 잡음
-                Fruit blackGrape("blackGrape");  //블랙 사파이어 객체 생성
+                Fruit blackGrape("BlackGrape");  //블랙 사파이어 객체 생성
                 blackGrape.sprite.setTexture(blackGrapeTexture);  //블랙 사파이어 이미지 할당
                 blackGrape.sprite.setPosition(1200, 1000);  //블랙 사파이어 위치 설정
                 fruits.push_back(blackGrape);  //벡터에 추가
@@ -233,15 +293,15 @@ void Game::run(RenderWindow& window) {
                 stick.setPosition(1200, 1000);  //원래 꼬치 멀리 보내버림
                 isStickGrabbed = true;  //꼬치 잡음
             }
-            else if (ladleSprite.getGlobalBounds().contains(static_cast<Vector2f>(mousePosition))) {  //국자
+            //과일을 다 꽂지 않으면 국자를 잡을 수 없음
+            else if (fruits.size() == maxSize && ladleSprite.getGlobalBounds().contains(static_cast<Vector2f>(mousePosition))) {  //국자
                 ladleSprite.setPosition(1200, 1000);  //원래 국자 멀리 보내버림
+                ladleSprite.setRotation(0);  //국자 각도 설정
                 isLadleGrabbed = true;  //국자 잡음
-                ladleSprite.setRotation(0);;  //국자 각도 설정
             }
         }
-
-        int positionX = 200;  //과일 x 위치 설정
-        int positionY = 580;  //과일 y 위치 설정
+        int positionX = 230;  //과일 x 위치 설정
+        int positionY = 560;  //과일 y 위치 설정
 
         // 마우스 이동 중일 때
         if (event.type == Event::MouseMoved) {
@@ -265,9 +325,8 @@ void Game::run(RenderWindow& window) {
                     Vector2f centerPosition(static_cast<float>(mousePosition.x - fruit.sprite.getLocalBounds().width / 2), static_cast<float>(mousePosition.y - fruit.sprite.getLocalBounds().height / 2));
                     fruit.sprite.setPosition(centerPosition);  //과일 잡는 위치 설정
                 }
-                if (isSugarLadle && ladleSprite.getGlobalBounds().intersects(fruit.sprite.getGlobalBounds())) {  //설탕물 국자일때 과일에 닿으면 코팅됨
+                if (isSugarLadle && ladleSprite.getGlobalBounds().intersects(fruit.sprite.getGlobalBounds()) && !fruit.isCoated) {  //설탕물 국자일때 과일에 닿으면 코팅됨
                     fruit.sugarCoating();  //설탕 코팅 이미지로 바꿈
-                    fruit.isCoating = true;  //코팅됨
                 }
             }
         }
@@ -285,7 +344,7 @@ void Game::run(RenderWindow& window) {
         if (event.type == Event::MouseButtonReleased && event.mouseButton.button == Mouse::Left) {
             isClicked = false;  //클릭 안함
             if (isStickGrabbed) {
-                stick.setPosition(40, 570);  //꼬치 위치 자동으로 설정
+                stick.setPosition(40, 550);  //꼬치 위치 자동으로 설정
                 isStickGrabbed = false;
             }
 
@@ -297,16 +356,10 @@ void Game::run(RenderWindow& window) {
                 }
                 else{
                     fruit.sprite.setPosition(positionX, positionY - fruit.sprite.getGlobalBounds().height / 2);  //과일 위치 자동으로 설정
-                    positionX += fruit.sprite.getGlobalBounds().width;  //가로 길이 누적
+                    positionX += fruit.sprite.getGlobalBounds().width-6;  //가로 길이 누적
                 }
             }
         }
-        /*for (Fruit& fruit : fruits) {
-            if (ladleSprite.getGlobalBounds().intersects(fruit.sprite.getGlobalBounds())) {
-                fruit.sugarCoating();  //설탕 코팅 이미지로 바꿈
-                fruit.isCoating = true;  //코팅됨
-            }
-        }*/
         
         //탕후루 게임 끝
 
@@ -343,7 +396,7 @@ void Game::run(RenderWindow& window) {
         if (Sale_btnVisible) {
             window.draw(Sale_btn_sprite);
         }
-        window.draw(ladleSprite);  //국자 test
+        window.draw(ladleSprite);  //국자
         window.draw(timerText); // 제한 시간 표시
         window.draw(bubbleText);//말풍선 말
         window.display();
